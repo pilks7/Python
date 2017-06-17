a0661c24f0b41e68d618a37427d23fb76f291eeb
Luke Pilkington <7pilks7@gmail.com>
2017-06-17 14:33:23 +0100

    Add files via upload

---
+++ boilerplate.py
@@ -0,0 +1,18 @@
+#boilerplate
+import tkinter as tk
+
+class App(tk.Frame):
+	def __init__(self, master):
+		tk.Frame.__init__(self, master)
+		self.pack()
+		self.master.title("Hello World")
+
+		tk.Label(self, text="This is your first GUI. (highfive)").pack()
+
+if __name__=='__main__':
+	root = tk.Tk()
+	app = App(root)
+	app.mainloop()
+
+
+
